cb33c7982eee8e38d1dd77a0ddb71bb383b7a6f7
Lakhwindermadhar <lakhwindermadhar95@gmail.com>
2026-06-04 11:01:47 -0400

    Add files via upload

---
+++ LakhwinderMadha_food.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  //include input-output library
+
+int main(void) // program startup
+{
+	// print the four favourite foods using printf command
+
+	printf("Butter Chicken\n");   
+	printf("Chicken Briyani\n");   
+	printf("Raja Rice\n");           
+	printf("Fish and chips\n");       
+
+	return 0;  // indicate that the program finished successfully
+}
